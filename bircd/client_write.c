5bd0bf5a5245feca6b90db74253fb31e92fa214b
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-11-01 17:08:46 -0300

    implemented client_write on bircd

--- bircd/client_write.c
+++ bircd/client_write.c
@@ -1,7 +1,16 @@
 
-#include <sys/socket.h>
 #include "bircd.h"
+#include <string.h>
+#include <sys/socket.h>
 
 void	client_write(t_env *e, int cs)
 {
+	int	len;
+
+	len = strlen(e->fds[cs].buf_write);
+	if (len > 0)
+	{
+		send(cs, e->fds[cs].buf_write, len, 0);
+		e->fds[cs].buf_write[0] = '\0';
+	}
 }
