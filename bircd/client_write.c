c166363e740ff02e7860aafef5d845626dca3928
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-11-01 18:58:50 -0300

    prints on client_write

--- bircd/client_write.c
+++ bircd/client_write.c
@@ -2,15 +2,19 @@
 #include "bircd.h"
 #include <string.h>
 #include <sys/socket.h>
+#include <stdio.h>
 
 void	client_write(t_env *e, int cs)
 {
 	int	len;
 
+	printf("called client_write\n");
+	printf("buf before send: %s\n", e->fds[cs].buf_write);
 	len = strlen(e->fds[cs].buf_write);
 	if (len > 0)
 	{
 		send(cs, e->fds[cs].buf_write, len, 0);
 		e->fds[cs].buf_write[0] = '\0';
+		printf("buf after send: %s\n", e->fds[cs].buf_write);
 	}
 }
